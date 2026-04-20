05acb0b594ca55c74f49bef7066dcd56e4bae247
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2026-04-20 15:57:06 -0400

    chore: fix unused import

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, PropsWithChildren } from "react";
+import React, { useState, useEffect } from "react";
 import { useStore, EventGroup, getMaxGroups, DecodedAppState } from "../store";
 import { format, isBefore } from "date-fns";
 import CalIcon from "./icons/CalIcon";
